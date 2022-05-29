f68a9ed8091ad62fc96393213e3bb94339645d77
Paula Lustosa <pl.lustosa@gmail.com>
2022-05-29 11:33:57 -0300

    Initial commit

---
+++ src/main/java/com/letscode/sales/SalesApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letscode/sales/SalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
