eaaafb84da385b77932d8f4b2b2cdd365ccc2215
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-05-30 17:25:31 -0300

    refactor: variavel renomeada | feat: criacao do construtor

--- src/main/java/com/letscode/sales/model/Product.java
+++ src/main/java/com/letscode/sales/model/Product.java
@@ -1,10 +1,25 @@
 package com.letscode.sales.model;
 
+import com.letscode.sales.dto.ProductClientResponse;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class Product {
   private String uuid;
   private String name;
   private BigDecimal price;
-  private int numberOfItemsInTheCart;
+  private int cartQuantity;
+
+  public Product (ProductClientResponse clientProductResponse){
+    this.uuid = clientProductResponse.getUuid();
+    this.name = clientProductResponse.getName();
+    this.price =  clientProductResponse.getPrice();
+  };
+
 }
