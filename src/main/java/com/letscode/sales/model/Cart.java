85b21ac077b4ddbbbb30ae2c245119ab78f27a1f
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-05-30 18:03:53 -0300

    refactor:Modificacao da model Cart

--- src/main/java/com/letscode/sales/model/Cart.java
+++ src/main/java/com/letscode/sales/model/Cart.java
@@ -1,11 +1,33 @@
 package com.letscode.sales.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@Document
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cart {
-  private Costumer costumer;
-  private List<Product> shoppingCart;
+  @Id
+  private ObjectId id;
+  private String uuid = UUID.randomUUID().toString();
+  private List<Product> shoppingCart = new ArrayList<>();
+
+  public Cart(Product product) {
+    this.shoppingCart.add(product);
+  }
+
+  public void updateShoppingCart(Product product) {
+    this.shoppingCart.add(product);
+  }
 }
