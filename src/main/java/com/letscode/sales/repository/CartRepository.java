a494c6392b2b8ed2fa93b9203bb20c61ff9f4c2f
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-05-29 22:01:09 -0300

    feat:Criacao do CartRepository

---
+++ src/main/java/com/letscode/sales/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package com.letscode.sales.repository;
+
+import com.letscode.sales.model.Cart;
+import org.bson.types.ObjectId;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+public interface CartRepository extends ReactiveCrudRepository<Cart, ObjectId> {
+  Mono<Cart> findByUuid(String uuid);
+}
