ecaf90bbc96c9bdfac75c9f0514e8d7c8628e659
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-05-30 18:21:12 -0300

    feat: adicao de atributos

--- src/main/java/com/letscode/sales/model/Customer.java
+++ src/main/java/com/letscode/sales/model/Customer.java
@@ -3,7 +3,10 @@ package com.letscode.sales.model;
 import lombok.Getter;
 
 @Getter
-public class Costumer {
+public class Customer {
   private String uuid;
+  private String name;
+  private String cpf;
+  private String email;
   private String adress;
 }
