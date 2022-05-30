1d1d324694501c2f7fbbbb079d299013176cba75
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-05-30 17:56:31 -0300

    refactor: Modificacao da classe CartResponse para retornar somente a mensagem

--- src/main/java/com/letscode/sales/DTO/CartResponse.java
+++ src/main/java/com/letscode/sales/DTO/CartResponse.java
@@ -1,23 +1,12 @@
-package com.letscode.sales.DTO;
+package com.letscode.sales.dto;
 
-import com.letscode.sales.model.Cart;
-import com.letscode.sales.model.Product;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.List;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class CartResponse {
-  private String uuid;
-  private List<Product> shoppingCart;
-  String message = "Product successfuly added to cart.";
-
-  public CartResponse(Cart cart) {
-    this.uuid = cart.getUuid();
-    this.shoppingCart = cart.getShoppingCart();
-  }
+  String message = "Product successfully added to cart.";
 }
