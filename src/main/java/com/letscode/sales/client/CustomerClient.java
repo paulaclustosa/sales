9c951b36ac1c921e7efcbfc7031b10281debeb67
Darccelio Wildson de Freitas e Silva <65990795+darccelio@users.noreply.github.com>
2022-06-02 18:37:17 -0300

    reafactor:feign implementation in productClient and CustomerClient

--- src/main/java/com/letscode/sales/client/CustomerClient.java
+++ src/main/java/com/letscode/sales/client/CustomerClient.java
@@ -1,26 +1,28 @@
 package com.letscode.sales.client;
 
-import com.letscode.sales.model.Customer;
-import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import com.letscode.sales.dto.CustomerClientResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@Service
-public class CustomerClient {
+@FeignClient(name = "user-api", url = "localhost:8080")
+public interface CustomerClient {
 
-  private WebClient webClient = WebClient.create("http://localhost:8080");
+  @GetMapping("/{uuid}")
+  CustomerClientResponse findUserByUuid(@PathVariable String uuid);
 
-  public Mono<Customer> findCustomerByUuid(String customerUuid) {
-    try {
-      Thread.sleep(2000);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
-    }
-    return webClient
-        .method(HttpMethod.GET)
-        .uri("/users/{customerUuid}", customerUuid)
-        .retrieve()
-        .bodyToMono(Customer.class);
-  }
+  //  private WebClient webClient = WebClient.create("http://localhost:8080");
+  //
+  //  public Mono<Customer> findCustomerByUuid(String customerUuid) {
+  //    try {
+  //      Thread.sleep(2000);
+  //    } catch (InterruptedException e) {
+  //      throw new RuntimeException(e);
+  //    }
+  //    return webClient
+  //        .method(HttpMethod.GET)
+  //        .uri("/users/{customerUuid}", customerUuid)
+  //        .retrieve()
+  //        .bodyToMono(Customer.class);
+  //  }
 }
--- src/main/java/com/letscode/sales/client/ProductClient.java
+++ src/main/java/com/letscode/sales/client/ProductClient.java
@@ -1,22 +1,25 @@
 package com.letscode.sales.client;
 
 import com.letscode.sales.dto.ProductClientResponse;
-import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@Service
-public class ProductClient {
+@FeignClient(name = "product-api", url = "localhost:8081")
+public interface ProductClient {
+  @GetMapping("/{uuid}/{quantity}")
+  ProductClientResponse getProductForSale(
+      @PathVariable("uuid") String uuid, @PathVariable("quantity") int quantity);
 
-  private WebClient webClient = WebClient.create("http://localhost:8081");
-
-  public Mono<ProductClientResponse> findProductByUuid(String productUuid, int quantity) {
-
-    return webClient.method(HttpMethod.GET)
-        .uri("/products/{productUuid}/{quantity}", productUuid, quantity)
-        .retrieve()
-        .bodyToMono(ProductClientResponse.class);
-  }
+  //
+  //  private WebClient webClient = WebClient.create("http://localhost:8081");
+  //
+  //  public Mono<ProductClientResponse> findProductByUuid(String productUuid, int quantity) {
+  //
+  //    return webClient.method(HttpMethod.GET)
+  //        .uri("/products/{productUuid}/{quantity}", productUuid, quantity)
+  //        .retrieve()
+  //        .bodyToMono(ProductClientResponse.class);
+  //  }
 
 }
