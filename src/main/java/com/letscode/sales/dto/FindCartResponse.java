8af48257158af804801132f560a70eda3e288610
Darccelio Wildson de Freitas e Silva <65990795+darccelio@users.noreply.github.com>
2022-06-02 17:55:33 -0300

    feat:createSale implementation

---
+++ src/main/java/com/letscode/sales/dto/FindCartResponse.java
@@ -0,0 +1,2 @@
+package com.letscode.sales.dto;public class FindCartResponse {
+}
--- src/main/java/com/letscode/sales/service/impl/SaleServiceImpl.java
+++ src/main/java/com/letscode/sales/service/impl/SaleServiceImpl.java
@@ -1,6 +1,11 @@
 package com.letscode.sales.service.impl;
 
+import com.letscode.sales.client.CustomerClient;
+import com.letscode.sales.dto.SaleResponse;
+import com.letscode.sales.model.Cart;
+import com.letscode.sales.model.Customer;
 import com.letscode.sales.model.Sale;
+import com.letscode.sales.repository.CartRepository;
 import com.letscode.sales.repository.SaleRepository;
 import com.letscode.sales.service.SaleService;
 import lombok.RequiredArgsConstructor;
@@ -12,19 +17,76 @@ import reactor.core.publisher.Mono;
 public class SaleServiceImpl implements SaleService {
 
   private final SaleRepository saleRepository;
+  private final CartRepository cartRepository;
+  private final CustomerClient customerClient;
 
   @Override
-  public Mono<Sale> createSale(String costumerUuid, String productUuid) {
-    return null;
-  }
+  public Mono<SaleResponse> createSale(String customerUuid, String cartUuid) {
 
-  @Override
-  public Mono<Void> cancelSale(String saleUuid) {
-    return null;
-  }
+    /*var a = customerClient.findCustomerByUuid(customerUuid)
+      .doOnNext(customer -> {
+        Sale createdSale = new Sale();
+        createdSale.setCustomer(customer);
+      }).flatMap(customer -> Mono.just(createdSale);
 
-  @Override
-  public Mono<Sale> finishSale(String saleUuid) {
-    return null;
+    }).*/
+
+    //    Mono<Sale> saleMono =
+    //        Mono.just(new Sale())
+    //            .doOnNext(
+    //                sale -> {
+    //                  Customer customer =
+    //                      customerClient
+    //                          .findCustomerByUuid(customerUuid)
+    //                          .doOnNext(sale::setCustomer)
+    //                          .block();
+    //
+    // .then(cartRepository.findByUuid(cartUuid).doOnNext(sale::setCart));
+    //                  customer
+    //                      .doAfterTerminate(
+    //                          (Runnable)
+    // cartRepository.findByUuid(cartUuid).doOnNext(sale::setCart))
+    //                      //                      .doAfterTerminate(
+    //                      //                          (Runnable)
+    //                      //
+    // cartRepository.findByUuid(cartUuid).doOnNext(sale::setCart))
+    //                      .subscribe();
+    //
+    //                  customer.subscribe();
+
+    //                  Cart shoppingCart =
+    //                      cartRepository.findByUuid(cartUuid).doOnNext(sale::setCart).block();
+
+    //                  sale.setCart(shoppingCart);
+
+    //                  saleRepository.save(sale).subscribe();
+    //                });
+
+    //    saleMono.subscribe();
+
+    //    Mono<Sale> sale = Mono.just(new Sale());
+    //        saleMono.subscribe();
+    Mono<Customer> customer = customerClient.findCustomerByUuid(customerUuid);
+    //      .doOnNext(sale::setCustomer);
+    //        customer.subscribe();
+
+    Mono<Cart> shoppingCart = cartRepository.findByUuid(cartUuid);
+    //            .doOnNext(sale::setCart);
+    //        shoppingCart.subscribe();
+
+    //    Mono<Sale> savedSale = saleRepository.save(sale);
+    //        savedSale.subscribe();
+
+    return Mono.zip(customer, shoppingCart)
+        .flatMap(
+            monos -> {
+              Customer customer1 = monos.getT1();
+              Cart cart1 = monos.getT2();
+              Sale sale = new Sale();
+              sale.setCustomer(customer1);
+              sale.setCart(cart1);
+              return saleRepository.save(sale);
+            })
+        .map(SaleResponse::new);
   }
 }
