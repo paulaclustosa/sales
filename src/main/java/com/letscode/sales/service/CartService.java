99b0941fab4fd63d104fb8ee2c84bbca63b3b8ea
Darccelio Wildson de Freitas e Silva <65990795+darccelio@users.noreply.github.com>
2022-06-02 18:07:55 -0300

    feat: crud methods impl

--- src/main/java/com/letscode/sales/service/CartService.java
+++ src/main/java/com/letscode/sales/service/CartService.java
@@ -2,13 +2,25 @@ package com.letscode.sales.service;
 
 import com.letscode.sales.dto.CartRequest;
 import com.letscode.sales.dto.CartResponse;
+import com.letscode.sales.dto.FindCartResponse;
+import com.letscode.sales.dto.RemoveItemFromCartRequest;
+import com.letscode.sales.model.Cart;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface CartService {
 
   public Mono<CartResponse> createCart(CartRequest request);
 
-  public Mono<CartResponse> addToCart(String cartUuid, CartRequest request);
+  public Mono<CartResponse> handleAddToCart(String cartUuid, CartRequest request);
 
-  public Mono<CartResponse> removeFromCart(CartRequest request);
+  public Mono<CartResponse> updateCart(String cartUuid, String productUuid, int quantity);
+
+  public Mono<CartResponse> removeItemFromCart(RemoveItemFromCartRequest removeItemFromCartRequest);
+
+  public Mono<Void> deleteCart(String cartUuid);
+
+  public Mono<Cart> findCartByUuid(String cartUuid);
+
+  public Flux<FindCartResponse> findAll();
 }
