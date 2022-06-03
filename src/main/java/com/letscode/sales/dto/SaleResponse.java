c0056783e1dfaa5b3931d82d82be673e5e6acc2d
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-06-03 17:37:42 -0300

    feat:criacao de contrutor personalizado e criacao dos atributos

--- src/main/java/com/letscode/sales/dto/SaleResponse.java
+++ src/main/java/com/letscode/sales/dto/SaleResponse.java
@@ -1,7 +1,26 @@
 package com.letscode.sales.dto;
 
+import com.letscode.sales.model.Cart;
+import com.letscode.sales.model.Customer;
 import com.letscode.sales.model.Sale;
+import com.letscode.sales.model.Status;
+import lombok.Getter;
 
+import java.time.LocalDateTime;
+
+@Getter
 public class SaleResponse {
-  public SaleResponse(Sale sale2) {}
+  private String uuid;
+  private Customer customer;
+  private Cart cart;
+  private Status status;
+  private LocalDateTime creationDate;
+
+  public SaleResponse(Sale sale) {
+    this.uuid = sale.getUuid();
+    this.customer = sale.getCustomer();
+    this.cart = sale.getCart();
+    this.status = sale.getStatus();
+    this.creationDate = sale.getCreationDate();
+  }
 }
