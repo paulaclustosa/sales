8fab84e5870bb5b2304fb6a8267710d21e8cdfc5
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-06-03 17:43:07 -0300

    style: remocao de codigo comentado

--- src/main/java/com/letscode/sales/client/CustomerClient.java
+++ src/main/java/com/letscode/sales/client/CustomerClient.java
@@ -5,28 +5,15 @@ import com.letscode.sales.dto.CustomerClientResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-// @FeignClient(name = "user-api", url = "localhost:8080")
-// public interface CustomerClient {
 @Service
 @RequiredArgsConstructor
 public class CustomerClient {
 
-  //  @GetMapping("/{uuid}")
-  //  Mono<CustomerClientResponse> findUserByUuid(@PathVariable String uuid);
-
-//  private WebClient webClient = WebClient.create("http://user-api");
-
   private final WebClientConfig webClient;
 
   public Mono<CustomerClientResponse> findUserByUuid(String customerUuid) {
-    //    try {
-    //      Thread.sleep(2000);
-    //    } catch (InterruptedException e) {
-    //      throw new RuntimeException(e);
-    //    }
     return webClient.getWebClientBuilder().baseUrl("http://user-api").build()
         .method(HttpMethod.GET)
         .uri("/users/{customerUuid}", customerUuid)
--- src/main/java/com/letscode/sales/client/ProductClient.java
+++ src/main/java/com/letscode/sales/client/ProductClient.java
@@ -5,20 +5,14 @@ import com.letscode.sales.dto.ProductClientResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-// @FeignClient(name = "product-api")
 @Service
 @RequiredArgsConstructor
 public class ProductClient {
-  //  @GetMapping("/{uuid}/{quantity}")
-  //  Mono<ProductClientResponse> findProductByUuid(
-  //      @PathVariable("uuid") String uuid, @PathVariable("quantity") int quantity);
-
-//  private WebClient webClient = WebClient.create("http://localhost:8081");
 
   private final WebClientConfig webClient;
+
   public Mono<ProductClientResponse> findProductByUuid(String productUuid, int quantity) {
     return webClient.getWebClientBuilder().baseUrl("http://product-api").build()
         .method(HttpMethod.GET)
