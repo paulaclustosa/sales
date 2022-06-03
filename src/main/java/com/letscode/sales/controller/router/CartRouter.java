641d9f2d183e388f8253f560c663b2dc1e18e1d5
Darccelio Wildson de Freitas e Silva <65990795+darccelio@users.noreply.github.com>
2022-06-03 12:13:46 -0300

    refactor:CartHandler and CartRouter refact

--- src/main/java/com/letscode/sales/controller/router/CartRouter.java
+++ src/main/java/com/letscode/sales/controller/router/CartRouter.java
@@ -1,13 +1,8 @@
 package com.letscode.sales.controller.router;
 
 import com.letscode.sales.controller.handler.CartHandler;
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springdoc.core.annotations.RouterOperation;
-import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -15,26 +10,26 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
-
-//@AllArgsConstructor
-//@RequiredArgsConstructor
+// @AllArgsConstructor
+// @RequiredArgsConstructor
 @Configuration
 public class CartRouter {
-
-//  CartHandler handler;
 
   @Bean
   public RouterFunction<ServerResponse> routeShoppingCart(CartHandler handler) {
     return route()
-        .path("/shopping-cart", b1 -> b1
-          .nest(accept(APPLICATION_JSON), b2 -> b2
-                  .POST("", handler::createCart)
-                  .GET("/{cartUuid}", handler::findCartByUuid)
-                  .GET("", handler::findAll)
-                  .DELETE("", handler::deleteCart)
-                  .PATCH("", handler::removeItemFromCart)
-                  .PATCH("/{cartUuid}", handler::updateCart)))
+        .path(
+            "/shopping-cart",
+            b1 ->
+                b1.nest(
+                    accept(APPLICATION_JSON),
+                    b2 ->
+                        b2.POST("", handler::createCart)
+                            .GET("/{cartUuid}", handler::findCartByUuid)
+                            .GET("", handler::findAll)
+                            .PATCH("/{cartUuid}", handler::handleAddToCart)
+                            .PATCH("", handler::removeItemFromCart)
+                            .DELETE("", handler::deleteCart)))
         .build();
   }
-
 }
