198f59b3cb2161cdb4e43e3129c5979e727d9c29
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-06-03 23:51:55 -0300

    criacao de classes de erro especificas

--- src/main/java/com/letscode/sales/dto/errors/BusinessValidationErrorResponse.java
+++ src/main/java/com/letscode/sales/dto/errors/BusinessValidationErrorResponse.java
@@ -1,2 +1,16 @@
-package com.letscode.sales.dto.errors;public class BusinessValidationErrorResponse {
+package com.letscode.sales.dto.errors;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+public class BusinessValidationErrorResponse {
+  private int code;
+  private LocalDateTime ocurrenceDate;
+  private String errorMessage;
 }
--- src/main/java/com/letscode/sales/dto/errors/FieldValidationErrorResponse.java
+++ src/main/java/com/letscode/sales/dto/errors/FieldValidationErrorResponse.java
@@ -1,2 +1,13 @@
-package com.letscode.sales.dto.errors;public class FieldValidationErrorResponse {
+package com.letscode.sales.dto.errors;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class FieldValidationErrorResponse {
+  private String field;
+  private String errorMessage;
 }
