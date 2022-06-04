6f8323833799288514c5a348d39848e0d4caccd7
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-06-03 22:20:20 -0300

    feat: adicionado circuit breaker

--- src/main/java/com/letscode/sales/client/CustomerClient.java
+++ src/main/java/com/letscode/sales/client/CustomerClient.java
@@ -2,6 +2,7 @@ package com.letscode.sales.client;
 
 import com.letscode.sales.config.WebClientConfig;
 import com.letscode.sales.dto.CustomerClientResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ public class CustomerClient {
 
   private final WebClientConfig webClient;
 
+  @CircuitBreaker(name = "costumer-cb")
   public Mono<CustomerClientResponse> findUserByUuid(String customerUuid) {
     return webClient.getWebClientBuilder().baseUrl("http://user-api").build()
         .method(HttpMethod.GET)
