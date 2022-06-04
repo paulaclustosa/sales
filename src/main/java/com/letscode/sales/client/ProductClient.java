a287637de0d87170819807cd591875b9f4a61bfe
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-06-03 22:47:51 -0300

    feat: conexao com a api de produtos e implementacao do circuit breaker

--- src/main/java/com/letscode/sales/client/ProductClient.java
+++ src/main/java/com/letscode/sales/client/ProductClient.java
@@ -1,23 +1,49 @@
 package com.letscode.sales.client;
 
 import com.letscode.sales.config.WebClientConfig;
+import com.letscode.sales.dto.CartListRequest;
 import com.letscode.sales.dto.ProductClientResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductClient {
 
   private final WebClientConfig webClient;
 
+  @CircuitBreaker(name = "product-cb")
   public Mono<ProductClientResponse> findProductByUuid(String productUuid, int quantity) {
     return webClient.getWebClientBuilder().baseUrl("http://product-api").build()
         .method(HttpMethod.GET)
         .uri("/products/{productUuid}/{quantity}", productUuid, quantity)
         .retrieve()
         .bodyToMono(ProductClientResponse.class);
+  }
+
+  @CircuitBreaker(name = "product-cb")
+  public Mono<String> removeItemsfromInventory(CartListRequest cartRequest) {
+    log.info("Entrei no client do Product para atualizar o inventário");
+
+//    return webClient.getWebClientBuilder().baseUrl("http://product-api").build()
+//        .method(HttpMethod.GET)
+//        .uri("/products/remove/{productUuid}/{quantity}", productUuid, quantity)
+//        .retrieve()
+//        .bodyToMono(String.class);
+
+    return webClient.getWebClientBuilder().baseUrl("http://product-api").build()
+        .method(HttpMethod.PUT)
+        .uri("/products/remove-items")
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(Mono.just(cartRequest), CartListRequest.class)
+        .retrieve()
+        .bodyToMono(String.class);
+        //.bodyToMono();
   }
 }
---
+++ src/main/java/com/letscode/sales/config/CircuitBreakerLogConfig.java
@@ -0,0 +1,41 @@
+package com.letscode.sales.config;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CircuitBreakerLogConfig {
+
+  public final Logger logger = LoggerFactory.getLogger(CircuitBreakerLogConfig.class);
+
+  @Bean
+  public RegistryEventConsumer<CircuitBreaker> cbLog(){
+    return new RegistryEventConsumer<CircuitBreaker>() {
+      @Override
+      public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+        entryAddedEvent.getAddedEntry()
+            .getEventPublisher()
+            .onStateTransition(event -> {
+              logger.info(event.toString());
+            });
+      }
+
+      @Override
+      public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+
+      }
+
+      @Override
+      public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+
+      }
+    };
+  }
+}
