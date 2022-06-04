870ce7ed797ea1d75bac5634da6ae70d27586ddc
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-06-03 22:55:23 -0300

    feat: implementacao da chamada na api de produtos

--- src/main/java/com/letscode/sales/service/impl/SaleServiceImpl.java
+++ src/main/java/com/letscode/sales/service/impl/SaleServiceImpl.java
@@ -1,17 +1,22 @@
 package com.letscode.sales.service.impl;
 
 import com.letscode.sales.client.CustomerClient;
-import com.letscode.sales.dto.CustomerClientResponse;
-import com.letscode.sales.dto.SaleResponse;
+import com.letscode.sales.client.ProductClient;
+import com.letscode.sales.dto.*;
 import com.letscode.sales.model.Cart;
 import com.letscode.sales.model.Customer;
 import com.letscode.sales.model.Sale;
+import com.letscode.sales.model.Status;
 import com.letscode.sales.repository.CartRepository;
 import com.letscode.sales.repository.SaleRepository;
 import com.letscode.sales.service.SaleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +25,12 @@ public class SaleServiceImpl implements SaleService {
   private final SaleRepository saleRepository;
   private final CartRepository cartRepository;
   private final CustomerClient customerClient;
+  private final ProductClient productClient;
 
   @Override
-  public Mono<SaleResponse> createSale(String customerUuid, String cartUuid) {
+  public Mono<SaleResponse> createSale(CreateSaleRequest createSaleRequest) {
+    String customerUuid = createSaleRequest.getCustomerUuid();
+    String cartUuid = createSaleRequest.getCartUuid();
 
     Mono<CustomerClientResponse> customerClientResponse =
         customerClient.findUserByUuid(customerUuid);
@@ -32,14 +40,37 @@ public class SaleServiceImpl implements SaleService {
     return Mono.zip(customerClientResponse, shoppingCart)
         .flatMap(
             monos -> {
-              CustomerClientResponse customer1 = monos.getT1();
-              Customer customer = new Customer(customer1);
+              CustomerClientResponse customerResponse = monos.getT1();
+              Customer customer = new Customer(customerResponse);
               Cart cart1 = monos.getT2();
+
+              List<CartRequest> productsOfCart = cartToCartRequestDto(cart1);
+              productClient.removeItemsfromInventory(new CartListRequest(productsOfCart)).subscribe();
               Sale sale = new Sale();
               sale.setCustomer(customer);
+              cart1.setStatus(Status.FINISHED);
               sale.setCart(cart1);
+
+//              removeItemsFromProduct(cart1);
               return saleRepository.save(sale);
             })
         .map(SaleResponse::new);
   }
+
+//  private void removeItemsFromProduct(Cart cart) {
+//    cart.getProducts().forEach((key, product) -> {
+//      productClient.removeItemsfromInventory(key, product.getCartQuantity()).subscribe();
+//    });
+//  }
+
+  private List<CartRequest> cartToCartRequestDto(Cart cart) {
+    List<CartRequest> cartRequestList = new ArrayList<>();
+    cart.getProducts().forEach((key, product) -> {
+
+      CartRequest cartRequest = new CartRequest(key, product.getCartQuantity());
+      cartRequestList.add(cartRequest);
+    });
+    return cartRequestList;
+  }
+
 }
