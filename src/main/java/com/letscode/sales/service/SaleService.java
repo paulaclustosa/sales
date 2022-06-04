b42a5f9cc1d50663a2ebd2e13d079adbd49fcf42
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-06-03 22:59:47 -0300

    feat: modificacao do parametro de entrada do metodo createSale

--- src/main/java/com/letscode/sales/service/SaleService.java
+++ src/main/java/com/letscode/sales/service/SaleService.java
@@ -1,10 +1,11 @@
 package com.letscode.sales.service;
 
+import com.letscode.sales.dto.CreateSaleRequest;
 import com.letscode.sales.dto.SaleResponse;
 import com.letscode.sales.model.Sale;
 import reactor.core.publisher.Mono;
 
 public interface SaleService {
-
-  Mono<SaleResponse> createSale(String costumerUuid, String cartUuid);
+  Mono<SaleResponse> createSale(CreateSaleRequest createSaleRequest);
+  //Mono<SaleResponse> createSale(String costumerUuid, String cartUuid);
 }
